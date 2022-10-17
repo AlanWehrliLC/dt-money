3ff9d7e967be51d2fac93ef89fb9e3ee684db4ab
AlanWehrliLC <wehrlialan1@gmail.com>
2022-10-16 21:30:45 -0300

    Creating table with received api data in context

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,34 +1,30 @@
+import { useContext } from "react"
+import { TransactionsContext } from "../../contexts/TransactionsContext"
 import { SearchForm } from "../SearchForm"
 import { PriceHighlight, TableContainer, TableContent } from "./styles"
 
 export function Table(){
+    const {transactions} = useContext(TransactionsContext)
+
     return (
         <TableContainer>
             <SearchForm />
             <TableContent>
                 <tbody>
-                    <tr>
-                        <td width="50%">Website development</td>
-                        <td>
-                            <PriceHighlight variant="income">
-                                $ 12.000,00
-                            </PriceHighlight>
-                        </td>
-                        <td>Sale</td>
-                        <td>10/05/2022</td>
-                    </tr>
-
-                    <tr>
-                        <td width="50%">Earphone</td>
-                        <td>
-                            <PriceHighlight variant="outcome">
-                                - $ 150,00
-                            </PriceHighlight>
-                        </td>
-                        <td>Items</td>
-                        <td>10/08/2022</td>
-                    </tr>
-
+                    {transactions.map(transaction => {
+                        return (
+                            <tr key={transaction.id}>
+                                <td width="50%">{transaction.description}</td>
+                                <td>
+                                    <PriceHighlight variant={transaction.type}>
+                                        $ {transaction.price}
+                                    </PriceHighlight>
+                                </td>
+                                <td>{transaction.category}</td>
+                                <td>{transaction.createdAt}</td>
+                            </tr>
+                        )
+                    })}
                 </tbody>
             </TableContent>
         </TableContainer>
