82523aaba1eab6fa8881996aae4857f847df18ba
AlanWehrliLC <wehrlialan1@gmail.com>
2022-10-16 22:04:20 -0300

    Formatting the data

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,5 +1,6 @@
 import { useContext } from "react"
 import { TransactionsContext } from "../../contexts/TransactionsContext"
+import { dataFormatter, priceFormatter } from "../../utils/formatter"
 import { SearchForm } from "../SearchForm"
 import { PriceHighlight, TableContainer, TableContent } from "./styles"
 
@@ -17,11 +18,12 @@ export function Table(){
                                 <td width="50%">{transaction.description}</td>
                                 <td>
                                     <PriceHighlight variant={transaction.type}>
-                                        $ {transaction.price}
+                                        {transaction.type === "outcome" && "- "}
+                                        {priceFormatter.format(transaction.price)}
                                     </PriceHighlight>
                                 </td>
                                 <td>{transaction.category}</td>
-                                <td>{transaction.createdAt}</td>
+                                <td>{dataFormatter.format(new Date(transaction.createdAt))}</td>
                             </tr>
                         )
                     })}
